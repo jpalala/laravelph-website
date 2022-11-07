925d5a1a540fed6b0c4888b471d128037845bf1d
Joe Palala <jose.palala@live.com>
2022-11-07 18:12:06 +0000

    much better

--- resources/views/welcome_to_laravelph.blade.php
+++ resources/views/welcome_to_laravelph.blade.php
@@ -9,14 +9,17 @@
             
             gtag('config', 'G-2B7FM57XC9');
         </script>
-        <meta charset="utf-8">
+	<meta charset="utf-8">
+	<meta name="description" content="LaravelPH is a community of Laravel Developers (Artisans) from the Philipines ">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>LaravelPH: Home </title>
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
         <!-- Styles -->
-        <link href="css/main.css" rel="stylesheet" type="text/css" />
-	<link href="css/agency.css" rel="stylesheet" type="text/css" />
+	<link href="css/pure.css" rel="stylesheet" type="text/css" />
+	<style>
+svg { width: 128px; height: 128px; fill: rgb(30,30,30); }
+	</style>
 <script>
 var hello = @php echo "'"; echo json_encode(array( 'test' => 'some shit')); echo "';";  @endphp
 console.log(hello);
@@ -49,25 +52,28 @@ console.log(hellotest.test);
             <div class="container">
                 <div class="flex-row d-flex justify-content-between">
                     <div class="col col-sm-3" style="margin-left: 15%;"a>
-                        <a href="https://www.facebook.com/groups/185448994945295">
-                        Facebook
+                        <a href="https://www.facebook.com/groups/185448994945295" title="facebook">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#fff" viewBox="0 0 24 24" class="icon-style"><title>facebook</title><path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"></path></svg>
                         </a>
                     </div>
                     <div class="col col-sm-3">
-                        <a href="https://discord.gg/dTWFd3z">Discord</a>
+			<a href="https://discord.gg/dTWFd3z" title="discord">
+<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#fff" viewBox="0 0 24 24" class="icon-style"><title>discord</title><path d="M20.317 4.3698a19.7913 19.7913 0 00-4.8851-1.5152.0741.0741 0 00-.0785.0371c-.211.3753-.4447.8648-.6083 1.2495-1.8447-.2762-3.68-.2762-5.4868 0-.1636-.3933-.4058-.8742-.6177-1.2495a.077.077 0 00-.0785-.037 19.7363 19.7363 0 00-4.8852 1.515.0699.0699 0 00-.0321.0277C.5334 9.0458-.319 13.5799.0992 18.0578a.0824.0824 0 00.0312.0561c2.0528 1.5076 4.0413 2.4228 5.9929 3.0294a.0777.0777 0 00.0842-.0276c.4616-.6304.8731-1.2952 1.226-1.9942a.076.076 0 00-.0416-.1057c-.6528-.2476-1.2743-.5495-1.8722-.8923a.077.077 0 01-.0076-.1277c.1258-.0943.2517-.1923.3718-.2914a.0743.0743 0 01.0776-.0105c3.9278 1.7933 8.18 1.7933 12.0614 0a.0739.0739 0 01.0785.0095c.1202.099.246.1981.3728.2924a.077.077 0 01-.0066.1276 12.2986 12.2986 0 01-1.873.8914.0766.0766 0 00-.0407.1067c.3604.698.7719 1.3628 1.225 1.9932a.076.076 0 00.0842.0286c1.961-.6067 3.9495-1.5219 6.0023-3.0294a.077.077 0 00.0313-.0552c.5004-5.177-.8382-9.6739-3.5485-13.6604a.061.061 0 00-.0312-.0286zM8.02 15.3312c-1.1825 0-2.1569-1.0857-2.1569-2.419 0-1.3332.9555-2.4189 2.157-2.4189 1.2108 0 2.1757 1.0952 2.1568 2.419 0 1.3332-.9555 2.4189-2.1569 2.4189zm7.9748 0c-1.1825 0-2.1569-1.0857-2.1569-2.419 0-1.3332.9554-2.4189 2.1569-2.4189 1.2108 0 2.1757 1.0952 2.1568 2.419 0 1.3332-.946 2.4189-2.1568 2.4189Z"></path></svg></a>
                     </div>
                     <div class="col col-sm-3">
-                        <a href="https://www.meetup.com/laravel-philippines/">Meetup.com</a>
+                        <a href="https://www.meetup.com/laravel-philippines/" title="Meetup"><img src="img/meetup.png" width="128" height="128"></a>
                     </div>
                     <div class="col col-sm-3">
-                        <a href="mailto:laravelph@gmail.com">Contact us via Email</a>
+			<a href="mailto:laravelph@gmail.com" title="Email us">
+<img src="img/emailicon.png" width="128" height="100">
+			</a>
                     </div>
                 </div>
             </div>
         </div>
         <div class="container">
             <div class="col-xs-12 lex justify-center pt-8 sm:justify-start sm:pt-0">
-                <p>LaravelPH is a community of laravel enthusiasts! We online meetups and have a bootcamp ur !</p>
+                <p>LaravelPH is a community of laravel enthusiasts! We have online meetups and trainings!</p>
             </div>
             <div class="footer" style="position:absolute; bottom: 2rem">
                 <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">LaravelPH &copy 2016- 2022 </div>
